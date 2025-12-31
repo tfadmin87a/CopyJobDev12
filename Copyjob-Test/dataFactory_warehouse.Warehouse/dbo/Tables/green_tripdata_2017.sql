2a5cb07a39cb9d356f54fe4877db65e604eeece0
Databricks <venkimhari@outlook.com>
2025-12-31 13:45:41 +0530

    Committing 7 items from workspace 7d80a729-a465-4561-bf89-4e6f75c88220

---
+++ Copyjob-Test/dataFactory_warehouse.Warehouse/dbo/Tables/green_tripdata_2017.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [dbo].[green_tripdata_2017] (
+
+	[VendorID] bigint NULL, 
+	[lpep_pickup_datetime] datetime2(6) NULL, 
+	[lpep_dropoff_datetime] datetime2(6) NULL, 
+	[store_and_fwd_flag] varchar(8000) NULL, 
+	[RatecodeID] bigint NULL, 
+	[PULocationID] bigint NULL, 
+	[DOLocationID] bigint NULL, 
+	[passenger_count] bigint NULL, 
+	[trip_distance] float NULL, 
+	[fare_amount] float NULL, 
+	[extra] float NULL, 
+	[mta_tax] float NULL, 
+	[tip_amount] float NULL, 
+	[tolls_amount] float NULL, 
+	[ehail_fee] float NULL, 
+	[improvement_surcharge] float NULL, 
+	[total_amount] float NULL, 
+	[payment_type] bigint NULL, 
+	[trip_type] bigint NULL, 
+	[congestion_surcharge] float NULL, 
+	[source_file] varchar(8000) NULL
+);
